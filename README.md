db5d265b69509a090331a7c17fa6109c135080af
MrRaptorious <35742529+MrRaptorious@users.noreply.github.com>
2025-04-10 15:59:58 +0000

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# haskellEnv
+
+
+
+
+Add this to settings.json in vscode:
+
+```json
+  "haskell.manageHLS": "GHCup",
+  "haskell.toolchain": {
+    "stack": null,
+    "ghc": "project",
+    "hls": "latest",
+    "cabal": "recommended"
+  }
+```
---
+++ postCreate.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# install random update
+sudo apt-get update -y
+sudo apt-get install libgmp-dev -y
+
+# update cache so random can be found
+stack update --allow-different-user
+
+# install random
+stack install random --allow-different-user
