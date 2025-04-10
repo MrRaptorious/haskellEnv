8ce52bfa595f899ca25f3a84be6aabe8d3d0ff5b
MrRaptorious <35742529+MrRaptorious@users.noreply.github.com>
2025-04-10 16:21:52 +0000

    ensure order of commands post create

--- postCreate.sh
+++ postCreate.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 # install random update
-sudo apt-get update -y
-sudo apt-get install libgmp-dev -y
+sudo apt-get update -y &&
+sudo apt-get install libgmp-dev -y && 
 
 # update cache so random can be found
-stack update --allow-different-user
+stack update --allow-different-user &&
 
 # install random
 stack install random --allow-different-user
